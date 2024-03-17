d59702a73efcc8689d21c2daa5b4440fdf925f01
Kluis6 <luislmorningstar@gmail.com>
2024-03-17 15:54:10 -0300

    check

--- src/components/home/cardproduct.tsx
+++ src/components/home/cardproduct.tsx
@@ -1,16 +1,30 @@
+"use client";
 import Link from "next/link";
-import React from "react";
-import { BsPlus } from "react-icons/bs";
+import React, { useState } from "react";
+import { BsCheckLg, BsPlusLg } from "react-icons/bs";
 
 export default function Cardproduct() {
+  const [confirm, setConfirm] = useState(false);
+
+  const addChart = () => {
+    setConfirm(true);
+  };
   return (
-    <Link href={""} className="hover:shadow-2xl">
-      <div className="relative flex flex-col overflow-hidden bg-white outline rounded outline-1 outline-slate-300 hover:shadow-xl transition-shadow duration-300 active:shadow">
-        {/* <Image src={""} alt={""} width={100} className="w-full h-[15rem]" /> */}
+    <div className="relative flex flex-col overflow-hidden bg-white outline rounded outline-1 outline-slate-300 hover:shadow-xl transition-shadow duration-300 active:shadow">
+      {/* <Image src={""} alt={""} width={100} className="w-full h-[15rem]" /> */}
+      <button
+        className="right-4 top-4 absolute bg-white rounded-full p-1.5 shadow-lg active:scale-90 transition-all duration-100  active:shadow z-30"
+        onClick={addChart}
+      >
+        {confirm ? (
+          <BsCheckLg size={20} className="text-green-700" />
+        ) : (
+          <BsPlusLg size={20} className="text-red-700" />
+        )}
+      </button>
+      <Link href={""}>
         <div className="w-full h-[15rem] bg-slate-500"></div>
-        <button className="right-4 top-4 absolute bg-white rounded-full p-1 shadow-lg">
-          <BsPlus size={20} className="text-green-700" />
-        </button>
+
         <div className="lg:px-4 px-2 py-4 space-y-4">
           <h4 className="text-sm truncate text-gray-700">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -22,7 +36,7 @@ export default function Cardproduct() {
             </p>
           </div>
         </div>
-      </div>
-   </Link>
+      </Link>
+    </div>
   );
 }
